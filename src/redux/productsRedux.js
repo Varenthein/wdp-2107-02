6528d7b3c858a6d73753b6e4f423ab09f0450b07
Deidrah <dominikastasik93@gmail.com>
2021-07-09 12:51:06 +0200

    WDP210702-21 -  Karta mebla - ocena w gwiazdkach

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,36 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* functions */
+
+const addMyRating = (state, action) => {
+  return state.map(currentStateElement => {
+    if (currentStateElement.id !== action.payload.id) {
+      return currentStateElement;
+    }
+    return {
+      ...currentStateElement,
+      myRating: action.payload.myRating,
+    };
+  });
+};
+
+/* action name creator */
+const reducerName = 'product';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+const ADD_RATING = createActionName('ADD_RATING');
+
+/* action creators */
+export const addRating = payload => ({ payload, type: ADD_RATING });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case ADD_RATING: {
+      return addMyRating(statePart, action);
+    }
     default:
       return statePart;
   }
